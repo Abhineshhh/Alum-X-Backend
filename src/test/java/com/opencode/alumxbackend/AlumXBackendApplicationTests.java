498a6fb564b148f6baee8619ab481534f267e8eb
Aryan9059 <99357111+Aryan9059@users.noreply.github.com>
2025-12-22 21:54:24 +0530

    Initial Setup

---
+++ src/test/java/com/opencode/alumxbackend/AlumXBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opencode.alumxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumXBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
